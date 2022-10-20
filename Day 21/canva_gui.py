1541f21ca1e1aa5a1a60d67d5782514b63360ef5
Gaurav Gupta <lomagaurav@gmail.com>
2022-10-20 19:37:29 +0530

    Canvas Code

--- Day 21/canva_gui.py
+++ Day 21/canva_gui.py
@@ -1,11 +1,21 @@
-import tkinter as tk
+import tkinter as ttk
+from tkinter import *
+from PIL import Image,ImageTk
 
-
-
+# Create an instance of tkinter frame
 win=Tk()
+
+#Set the geometry of tkinter frame
 win.geometry('750x270')
 
+#Create a canvas 
 canvas=Canvas(win,width=600,height=400)
 canvas.pack()
 
-img=ImageTk.PhotoImage(image.open(''))
+#Load an image in the script
+img=ImageTk.PhotoImage(Image.open(r'Day 21/images.png'))
+
+#Add image to the Canvas items
+canvas.create_image(10,10,anchor=NW,image=img)
+
+win.mainloop()
